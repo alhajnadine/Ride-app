c45bc323a4f9676828ee3d5beb1c93882bae779c
alhajnadine <62381122+alhajnadine@users.noreply.github.com>
2022-05-28 19:27:16 +0200

    f

---
+++ app/routes.js
@@ -0,0 +1,115 @@
+var mysql = require('mysql');
+var dbconfig = require('../config/database');
+var connection = mysql.createConnection(dbconfig.connection);
+connection.query('USE ' + dbconfig.database);
+
+
+module.exports = function(app, passport) {
+    app.get('/', function(req, res){
+     res.render('index.ejs');
+    });
+   
+    app.get('/login', function(req, res){
+     res.render('login.ejs', {message:req.flash('loginMessage')});
+    });
+   
+    app.post('/login', passport.authenticate('local-login', {
+     successReturnToOrRedirect: '..',
+     failureRedirect: '/login',
+     failureFlash: true
+    }),
+     function(req, res){
+      if(req.body.remember){
+       req.session.cookie.maxAge = 1000 * 60 * 3;
+      }else{
+       req.session.cookie.expires = false;
+      }
+      res.redirect('/');
+     });
+   
+    app.get('/signup', function(req, res){
+     res.render('signup.ejs', {message: req.flash('signupMessage')});
+    });
+   
+    app.post('/signup', passport.authenticate('local-signup', {
+     successRedirect: '/profil',
+     failureRedirect: '/signup',
+     failureFlash: true
+    }));
+   
+    app.get('/profil', isLoggedIn, function(req, res){
+     res.render('profil.ejs', {
+      user:req.user
+     });
+    });
+
+
+    app.get('/favoris/:id', isLoggedIn, function(req, res){
+        if(req.user) {
+            mail = req.user.username;
+            let select = `SELECT * FROM favoris WHERE mail = '${mail}' AND ActivitesId = ${req.params.id}`;
+            connection.query(select,(err, rows) => {
+                if(err) throw err;
+                else if(rows.length){
+                    console.log("activité déjà en favorite!")
+                    res.redirect('back');
+                }else {
+                    let sql = `INSERT INTO favoris VALUES ('${mail}',${req.params.id})`;
+                    connection.query(sql, (err) => {
+                        if(err) throw err;
+                        console.log("activité ajoutée en favoris!");
+                        res.redirect('back');
+                    });
+                }
+            });
+        } 
+        else {
+            res.redirect('/login');
+        }
+    });
+
+    app.get('/mesfavoris', isLoggedIn, function(req, res){
+        if(req.user) {
+            mail = req.user.username;
+            let select =`SELECT * FROM activites,favoris WHERE favoris.mail = '${mail}' AND activites.ActivitesId=favoris.ActivitesId;`
+            connection.query(select,(err, rows) => {
+                if(err) throw err;
+                else {
+                    var data = rows;
+                    res.render('favoris', {data:data});
+                }
+            });
+        }
+        else {
+            res.redirect('/login');
+        }
+    });
+
+    app.post('/commentaire/:id', isLoggedIn, function(req, res){
+        if(req.user) {
+            commentaire = req.body.commentaire;
+            mail = req.user.username;
+            let sql = `INSERT INTO commentaire(username,ActivitesId,commentaire) VALUES  ('${mail}','${req.params.id}', '${commentaire}')`;
+            connection.query(sql,(err, rows) => {
+                if(err) throw err;
+                console.log(req.body.commentaire);
+                console.log("commentaire ajoutée!");
+                res.redirect('back');
+            });
+        }
+        else {
+            res.redirect('/login');
+        }
+    });
+
+    app.get('/logout', function(req,res){
+     req.logout();
+     res.redirect('/');
+    })
+   };
+
+   function isLoggedIn(req, res, next){
+    if(req.isAuthenticated())
+     return next();
+    res.redirect('/login');
+   }
---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'connection':{
+     'host':'localhost',
+     'user':'root',
+     'password':'',
+     'port': '3306',
+    },
+    'database':'ride_app',
+    'user_table':'users'
+   }
